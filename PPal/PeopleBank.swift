f2fb66e8616d40df7fdb47fe0812124b842e34d6
Harry Gong <harryg@asb9700-f02.csil.sfu.ca>
2017-10-20 14:57:16 -0700

    Add PeopleBank

---
+++ PPal/PeopleBank.swift
@@ -0,0 +1,24 @@
+//
+//  PeopleBank.swift
+//  PPal
+//
+//  Created by Harry Gong on 10/20/17.
+//  Copyright © 2017 CMPT275. All rights reserved.
+//
+
+import Foundation
+class PeopleBank{
+    
+    //var people: [Person]
+    
+    func sort() {}
+    func add() -> Bool{}//bool
+    func del() -> Bool{}//bool
+   // func getlist() -> [People]//[people]
+    func getcount() -> Int{} //int
+    
+    
+    
+    
+    
+}
