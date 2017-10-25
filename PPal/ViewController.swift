9c99f80917518151076003e0dee581bbb777fe1a
Mirac Chen <yysxqq@hotmail.com>
2017-10-24 22:32:38 -0700

    Update ViewController.swift

--- PPal/ViewController.swift
+++ PPal/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var database: Connection!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do {
+            //create document and path URL
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory, 
+                in: .userDomainMask, appropriateFor: nil, create: true)
+            let fileUrl = documentDirectory.appendingPathComponent("person").appendingPathExtension("sqlite3") //table name to be determined
+            //make connection and create database
+            let database = try Connection(fileUrl.path)
+            self.database = database
+            
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
