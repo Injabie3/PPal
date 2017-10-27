054b735100ff03e70353d5c3d59c0a9345b96f45
Mirac Chen <yysxqq@hotmail.com>
2017-10-27 15:40:40 -0700

    Moved table creation from AppDelegate
    -Need to check for variable name conflict

--- PPal/Person.swift
+++ PPal/Person.swift
@@ -26,8 +26,38 @@ class Person {
     private var hasHouseKeys: Bool = false
     //private var labels: [Label]
     
-    var database: Connection!
+    //Table variable declaration
+    let personsTable = Table("persons")
+    let id = Expression<Int>("id")
+    let pathToPhoto = Expression<String>("pathToPhoto")
+    let firstName = Expression<String>("firstName")
+    let lastName = Expression<String>("lastName")
+    let phoneNumber = Expression<String>("phoneNumber")
+    let email = Expression<String>("email")
+    let address = Expression<String>("address")
+    let hasHouseKeys = Expression<Bool>("hasHouseKeys")
+    //we'll need to discuss how to store the labels ***
+
+
+    //Establish connection to the database file
+    let database = try Connection(fileUrl.path) //***** "fileUrl.path" needs to be replaced with reaql path <String>
     
+    //Table creation
+    //Which variables needs to be unique? needs to be discussed _mirac
+    
+    try database.run(personsTable.create { (table) in
+        table.column(self.id, primaryKey: true)
+        table.column(self.pathToPhoto, unique: true)
+        table.column(self.firstName)
+        table.column(self.lastName)
+        table.column(self.phoneNumber, unique: true)
+        table.column(self.email, unique: true)
+        table.column(self.address)
+        table.column(self.hasHouseKeys)
+        //again missing labels here***
+    })
+
+
     func getName() -> String {
         if (firstName == "" || lastName == "") {
             return ""
@@ -63,12 +93,7 @@ class Person {
             self.hasHouseKeys = hasHouseKeys
             
             
-            //make connection to database
-            let database = try Connection(fileUrl.path) //***** "fileUrl.path" needs to be replaced
-            self.database = database
-            
             //save profile entry to database
-            //***REPLACE "self.personsTable" so that it refers to personsTable in "class AppDelegate"
             let saveProfile = self.personsTable.insert(self.pathToPhoto <- pathToPhoto, self.firstName <- firstName,
                self.lastName <- lastName, self.phoneNumber <- phoneNumber, self.email <- email, self.address <- address,
                self.hasHouseKeys <- hasHouseKeys)
@@ -77,8 +102,7 @@ class Person {
                 print("Profile saved")
             } catch {
                 print(error)
-            }
-            
+            }   
             
             return true
             
