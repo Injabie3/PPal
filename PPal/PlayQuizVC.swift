aebc3bd2cf13dadb403e6d594deb845a9006ae95
Maple Tan <maplet@asb9700-e05.csil.sfu.ca>
2017-11-16 20:11:04 -0800

    Fix quiz portion. Buttons now show up highlighted red or green, depending on correctness. End quiz screen gives option to go back to home screen or to play the quiz again.

--- PPal/PlayQuizVC.swift
+++ PPal/PlayQuizVC.swift
@@ -23,20 +23,72 @@ class PlayQuizVC: UIViewController {
     var rightAnswerPlacement: UInt32 = 0
     var points = 0
     
+    @IBOutlet weak var resultText: UILabel!
     @IBOutlet weak var questionText: UILabel!
     @IBOutlet weak var quizPhoto: UIImageView!
-    @IBOutlet weak var answerButton: UIButton!
+    @IBOutlet weak var reviewButton: UIButton!
+    @IBOutlet weak var nextQuestionButton: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.reviewButton.isHidden = true
+        self.nextQuestionButton.isHidden = true
+        self.resultText.isHidden = true
+    }
+    
+    // If any answer is chosen, check if the answer is right
     @IBAction func buttonAction(_ sender: AnyObject) {
+        
         if sender.tag == Int(rightAnswerPlacement) {
             print("right answer")
+            resultText.text = "Correct"
+            resultText.textColor = UIColor(red: 0.06, green: 0.74, blue: 0.46, alpha: 1.0)
+            self.resultText.isHidden = false
+            self.self.view.viewWithTag(Int(rightAnswerPlacement))?.backgroundColor = UIColor(red: 0.06, green: 0.74, blue: 0.46, alpha: 1.0)
+            
+            // Uncomment if you want other answers to disappear
+            /*for i in 1...4 {
+                if (i != (Int(rightAnswerPlacement))) {
+                    print(i)
+                    self.self.view.viewWithTag(i)?.isHidden = true
+                }
+            }*/
             points += 1
         }
         else {
             print("wrong answer")
+            resultText.text = "Incorrect"
+            resultText.textColor = UIColor.red
+            self.resultText.isHidden = false
+            self.self.view.viewWithTag(Int(rightAnswerPlacement))?.backgroundColor = UIColor(red: 0.06, green: 0.74, blue: 0.46, alpha: 1.0)
+            self.self.view.viewWithTag(sender.tag)?.backgroundColor = UIColor.red
+
+            // Uncomment if you want other answers to disappear
+            /*for i in 1...4 {
+                if (i != sender.tag) && (i != (Int(rightAnswerPlacement))) {
+                    print(i)
+                    self.self.view.viewWithTag(i)?.isHidden = true
+                }
+            }*/
         }
         
+        hiddenButtons()
+    }
+    
+    // Pressing the next question button will
+    @IBAction func nextQuestion(_ sender: AnyObject) {
+        self.resultText.isHidden = true
+        hiddenButtons()
         if currentQuestion != questions.count {
+            for i in 1...4 {
+                // Uncomment if you want other answers to disappear
+                /*if (self.self.view.viewWithTag(i)?.isHidden)! {
+                    self.self.view.viewWithTag(i)?.isHidden = !((self.self.view.viewWithTag(i)?.isHidden)!)
+                }*/
+                
+                self.self.view.viewWithTag(i)?.backgroundColor = UIColor(red: 0.50, green: 0.52, blue: 1.00, alpha: 1.0)
+            }
             newQuestion()
         }
         else {
@@ -44,13 +96,21 @@ class PlayQuizVC: UIViewController {
         }
     }
     
+    func hiddenButtons() {
+        self.reviewButton.isHidden = !self.reviewButton.isHidden
+        self.nextQuestionButton.isHidden = !self.nextQuestionButton.isHidden
+    }
+
     override func viewDidAppear(_ animated: Bool) {
         newQuestion()
     }
     
+    // When currentQuestion != question.count, create a new question
     func newQuestion() {
         questionText.text = questions[currentQuestion]
         quizPhoto.image = UIImage(named: photos[currentQuestion])
+        
+        // Randomizes the placement of the correct answer
         rightAnswerPlacement = arc4random_uniform(4) + 1
         
         var button: UIButton = UIButton()
@@ -59,6 +119,7 @@ class PlayQuizVC: UIViewController {
         for i in 1...4 {
             button = view.viewWithTag(i) as! UIButton
             
+            // If i equals the randomly generated number from arc4random, then set the button with tag i as the correct answer
             if i == Int(rightAnswerPlacement) {
                 button.setTitle(answers[currentQuestion][0], for: .normal)
             }
@@ -68,6 +129,10 @@ class PlayQuizVC: UIViewController {
             }
         }
         currentQuestion += 1
+        
+        if currentQuestion == questions.count {
+            nextQuestionButton.setTitle("End Quiz", for: .normal)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -75,14 +140,6 @@ class PlayQuizVC: UIViewController {
         if segue.identifier == "segueToEndQuiz" {
             let viewVC = segue.destination as! EndQuizVC
             viewVC.endPoints = points
-            
         }
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
 }
